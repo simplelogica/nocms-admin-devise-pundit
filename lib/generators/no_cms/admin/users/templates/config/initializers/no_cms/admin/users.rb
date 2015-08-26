01fba629863b69e6b3c753d711d8df75a3145359
David Brenes <davidjbrenes@gmail.com>
2015-08-26 23:49:08 +0200

    Configuration to enable authentication in the admin

--- lib/generators/no_cms/admin/users/templates/config/initializers/no_cms/admin/users.rb
+++ lib/generators/no_cms/admin/users/templates/config/initializers/no_cms/admin/users.rb
@@ -4,4 +4,7 @@ NoCms::Admin::Users.configure do |config|
   # :confirmable, :lockable, :timeoutable and :omniauthable
   config.devise_modules = [:database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable]
 
+  # While you are in development you may consider to remove
+  # authentication just in order to work more comfortably
+  # config.authentication_enabled = true
 end
--- lib/no_cms/admin/users/configuration.rb
+++ lib/no_cms/admin/users/configuration.rb
@@ -2,7 +2,9 @@ module NoCms::Admin::Users
   include ActiveSupport::Configurable
 
     config_accessor :devise_modules
+    config_accessor :authentication_enabled
 
     self.devise_modules = [:database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable]
+    self.authentication_enabled = true
 
 end
