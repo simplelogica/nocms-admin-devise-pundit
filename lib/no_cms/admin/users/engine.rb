c496cc2d67ffb1b09a8bb5b5e9e59eb0b946e43e
David Brenes <davidjbrenes@gmail.com>
2015-08-26 23:49:33 +0200

    Deecorating the NoCms::Admin::AplicationController to include authentication

--- lib/no_cms/admin/users/engine.rb
+++ lib/no_cms/admin/users/engine.rb
@@ -4,5 +4,12 @@ require 'devise'
 module NoCms::Admin::Users
   class Engine < ::Rails::Engine
     isolate_namespace NoCms::Admin::Users
+
+    config.to_prepare do
+      Dir.glob(NoCms::Admin::Users::Engine.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+
   end
 end
