6d2afe4584e6fd94ea94009f0020ea01e1c54436
David Brenes <davidjbrenes@gmail.com>
2015-08-26 23:27:11 +0200

    Devise included in the model
    Previous create migration is changed so the email column includes all the restrictions from the original devise migration

--- app/models/no_cms/admin/users/user.rb
+++ app/models/no_cms/admin/users/user.rb
@@ -1,4 +1,8 @@
 module NoCms::Admin::Users
   class User < ActiveRecord::Base
+        # Include default devise modules. Others available are:
+        # :confirmable, :lockable, :timeoutable and :omniauthable
+        devise :database_authenticatable, :registerable,
+               :recoverable, :rememberable, :trackable, :validatable
   end
 end
