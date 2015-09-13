6f691974be8a3af0ad3827997d348a5a095e6bf0
David Brenes <davidjbrenes@gmail.com>
2015-09-13 12:26:35 +0200

    Fields for editing the password and processing them in the controller

--- app/controllers/no_cms/admin/users/users_controller.rb
+++ app/controllers/no_cms/admin/users/users_controller.rb
@@ -63,7 +63,10 @@ module NoCms::Admin::Users
     end
 
     def user_params
-      user_params = params.require(:user).permit(:email, role_ids: [])
+      user_params = params.require(:user).permit(:email, :password, :password_confirmation, role_ids: [])
+      user_params.delete(:password) if user_params[:password].blank?
+      user_params.delete(:password_confirmation) if user_params[:password_confirmation].blank?
+      user_params
     end
 
   end
